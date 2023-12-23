54eb3808bc659edba39cccb8cef76cc510cf9a15
Victor Lopez <stevelopezc@yahoo.com>
2023-12-22 18:22:34 -0600

    2

---
+++ ProjectTest.API/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace ProjectTest.API.Controllers
+{
+    [Route("")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _configuration;
+
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            _logger.LogInformation("Initial Home Get");
+            return $"Demo Travis - Net Core 5.0 v2.1.1 Running ... {_configuration["Company:Name"]} - {_configuration["Company:Author"]}";
+        }
+    }
+}
--- ProjectTest.API/Program.cs
+++ ProjectTest.API/Program.cs
@@ -5,20 +5,19 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
-//var port = Environment.GetEnvironmentVariable("PORT")??"8080";
+var port = Environment.GetEnvironmentVariable("PORT")??"8080";
 
 app.MapGet("/", () =>
 {
     app.Logger.LogInformation("Initial Home Get");
-    return "GET - Net 6 Running ... 15092023-v1.0.0 CI/CD in AKS v1.0.0 POLYGLOT";
+    return "GET - Net 6 Running ... 22122023-v1.0.0 CI/CD with TRAVISCI JOB v1.0.10 POLYGLOT TRAINNER FRANCO";
 });
 
 app.MapPost("/", () =>
 {
     app.Logger.LogInformation("Initial Home Post");
-    return "POST - Net 6 Running ...15092023-v1.0.0 CI/CD in AKS v1.0.0 POLYGLOT";
+    return "POST - Net 6 Running ... 20102023-v1.0.0 CI/CD in AKS v1.0.0 POLYGLOT TRAINNER FRANCO";
 });
 
-//app.Run($"http://0.0.0.0:{port}");
-app.Run();
+app.Run($"http://0.0.0.0:{port}");
+
